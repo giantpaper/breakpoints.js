8d808c0ab91a4391b073b5f51089575b739ae861
giantpaper <>
2022-04-07 19:43:05 -0700

    Debugging

--- demo.js
+++ demo.js
@@ -6,7 +6,7 @@ let breakpoints = new Breakpoints();
 let run = () => {
 	document.getElementById('log').innerText = 'Current breakpoint: ' + breakpoints.get();
 	document.getElementById('lte').innerText = 'x3 <= ' + breakpoints.lte('x3');
-	document.getElementById('gte').innerText = 'x3 >= ' + breakpoints.gte('x3');
+	document.getElementById('gte').innerText = 'lg >= ' + breakpoints.gte('lg');
 	document.getElementById('is').innerText = 'x3 === ' + breakpoints.is('x3');
 };
 
