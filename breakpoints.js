e0684324e2ce18b6299cd233f055df8e780d9761
giantpaper <giantpaper@mailbox.org>
2026-08-10 01:39:08 -0700

    IMPROV: remove empty lines

--- breakpoints.js
+++ breakpoints.js
@@ -1,6 +1,5 @@
 export default class Breakpoints {
 	constructor(config) {
-		
 		// If no custom breakpoints are set, use these defaults
 		this.config = config || [
 			[	'xs',	0	],
@@ -35,7 +34,6 @@ export default class Breakpoints {
 			}
 		});
 	}
-	
 	getKeyByValue(object, value) {
 	  return Object.keys(object).find(key => object[key] === value);
 	}
