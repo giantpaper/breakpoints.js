26663a7cdde3f5a3a46c4906ef411dacbdee9ab5
giantpaper <giantpaper@mailbox.org>
2026-08-12 02:41:51 -0700

    DOC: add comments for future!me & other devs

--- breakpoints.js
+++ breakpoints.js
@@ -12,8 +12,13 @@ export default class Breakpoints {
 			[	'x4',	1920	],
 			[	'x5',	2560	],
 		];
+		// Building the list of breakpoints with booleans as values
+		// (depending of if it's the current breakpoint or not)
 		this.b = {};
+		// Building the current list of each breakpoints max-width
+		// In this format -- key: value
 		this.n = {};
+		// Current window w -- set to 0 for now
 		this.w = 0;
 		
 		// Define the lastWindowW now...which will be....nothing if the page just loaded
@@ -29,11 +34,16 @@ export default class Breakpoints {
 	
 	// Debounce helper
 	debounce(func, delay) {
+		// Define `timeout`
 		let timeout;
 		return function() {
+			// Takes `this` == puts in a variable `context`
 			const context = this;
+			// `arguments` shortened to `args`
 			const args = arguments;
+			// Clear timeout
 			clearTimeout(timeout);
+			// Define timeout again, wait 100ms then run it
 			timeout = setTimeout(() => {
 				func.apply(context, args);
 			}, delay);
@@ -53,15 +63,27 @@ export default class Breakpoints {
 		// And this.lastWindowW with the current width also, which will become
 		// the previous width on next resize
 		this.lastWindowW = currentWindowW;
+		// Go through the entire list of breakpoints
 		this.config.forEach((v, k, a) => {
+			// This breakpoint label
 			let label = v[0];
+			// This breakpoint value
 			let res = v[1];
+			// Check if there is another breakpoint set after the current one
 			if (a[k+1] !== undefined) {
+				// If so, set its value as `next`
 				let next = a[k+1][1];
+				// Set max width for breakpoint
 				this.n[label] = res;
+				// true = if it's the current breakpoint
+				// false = if not
 				this.b[label] = res <= currentWindowW && currentWindowW < next;
+			// Resorts to this else if it's the last breakpoint in the list
 			} else {
+				// Set max width for breakpoint
 				this.n[label] = res;
+				// true = if it's the current breakpoint
+				// false = if not
 				this.b[label] = res <= currentWindowW;
 			}
 		});
@@ -71,11 +93,17 @@ export default class Breakpoints {
 	}
 	
 	lte(test) {
+		// All these redundant this.setValues() in this method and the below
+		// will only run if window width has changed
 		this.setValues();
+		// In this method and the below, check if the breakpoint actually exists
+		// If not, output an error
 		if (this.n[test] === undefined) {
 			console.warn(`Breakpoint label \`${test}\` not found.`);
 			return false;
 		}
+		// true = if this.w/window width is the current breakpoint or a lesser one
+		// false = if this.w/window width is greater than the current breakpoint
 		return this.w <= this.n[test] || this.b[test];
 	}
 	gte(test) {
@@ -84,6 +112,8 @@ export default class Breakpoints {
 			console.warn(`Breakpoint label \`${test}\` not found.`);
 			return false;
 		}
+		// true = if this.w/window width is the current breakpoint or a greater one
+		// false = if this.w/window width is lesser than the current breakpoint
 		return this.w >= this.n[test];
 	}
 	is(test) {
@@ -92,10 +122,13 @@ export default class Breakpoints {
 			console.warn(`Breakpoint label \`${test}\` not found.`);
 			return false;
 		}
+		// true = if this.w/window width is the current breakpoint
+		// false = if this.w/window width is not the current breakpoint
 		return this.b[test];
 	}
 	get() {
 		this.setValues();
+		// returns the string of current breakpoint label
 		return this.getKeyByValue(this.b, true);
 	}
 	between(test, test2) {
@@ -110,6 +143,9 @@ export default class Breakpoints {
 			}
 			return false;
 		}
+		// true = if this.w/window width is one of the two defined breakpoints
+		// or in between them
+		// false = if this.w/window width is outside of the breakpoint range
 		return this.n[test] <= this.w && this.w <= this.n[test2];
 	}
 	destroy() {
